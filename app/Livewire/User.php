18bea4d7b20364571a6dae706bf4995a54b5206c
ricky-hrf <riskiharefa2000@gmail.com>
2025-02-06 23:53:37 +0700

    membuat fitur tambah pengguna

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -3,17 +3,19 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User as ModelUser;
 
 class User extends Component
 {
     public $pilihanMenu = 'lihat';
-    public $semuaPengguna;
     public function pilihMenu($menu)
     {
         $this->pilihanMenu = $menu;
     }
     public function render()
     {
-        return view('livewire.user');
+        return view('livewire.user')->with([
+            'semuaPengguna' => ModelUser::all()
+        ]);
     }
 }
