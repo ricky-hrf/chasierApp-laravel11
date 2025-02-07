3feee515ef152cef8c28da8969321e1819b1d613
ricky-hrf <riskiharefa2000@gmail.com>
2025-02-07 22:02:18 +0700

    Memperbaiki kode fitur create pengguna dan menambah fitur tambah barang

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -8,6 +8,9 @@ use App\Models\User as ModelUser;
 class User extends Component
 {
     public $pilihanMenu = 'lihat';
+    public $name, $email, $password, $peran;
+    public $penggunaTerpilih;
+
     public function pilihMenu($menu)
     {
         $this->pilihanMenu = $menu;
@@ -17,5 +20,85 @@ class User extends Component
         return view('livewire.user')->with([
             'semuaPengguna' => ModelUser::all()
         ]);
+    }
+
+    public function simpan()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => 'required',
+            'peran' => 'required'
+        ], [
+            'name.required' => 'Nama tidak boleh kosong',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email tidak valid',
+            'email.unique' => 'Email sudah terdaftar',
+            'password.required' => 'Password tidak boleh kosong',
+            'peran.required' => 'Peran tidak boleh kosong'
+        ]);
+
+        $simpan = new ModelUser;
+        $simpan->name = $this->name;
+        $simpan->email = $this->email;
+        $simpan->password = bcrypt($this->password);
+        $simpan->peran = $this->peran;
+        $simpan->save();
+
+        $this->reset(['name', 'email', 'password', 'peran']);
+        $this->pilihanMenu = 'lihat';
+    }
+
+    public function hapus($id)
+    {
+        $this->penggunaTerpilih = ModelUser::findOrFail($id);
+        $this->pilihanMenu = 'hapus';
+    }
+
+    public function delete()
+    {
+        $this->penggunaTerpilih->delete();
+        $this->reset();
+    }
+
+    public function cancel()
+    {
+        $this->reset();
+    }
+
+    public function edit($id)
+    {
+        $this->penggunaTerpilih = ModelUser::findOrFail($id);
+        $this->name = $this->penggunaTerpilih->name;
+        $this->email = $this->penggunaTerpilih->email;
+        $this->peran = $this->penggunaTerpilih->peran;
+        $this->pilihanMenu = 'edit';
+    }
+
+    public function simpanPerubahan()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users,email,' . $this->penggunaTerpilih->id],
+            'peran' => 'required'
+        ], [
+            'name.required' => 'Nama tidak boleh kosong',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email tidak valid',
+            'email.unique' => 'Email sudah terdaftar',
+            'peran.required' => 'Peran tidak boleh kosong'
+        ]);
+
+        $simpan = $this->penggunaTerpilih;
+        $simpan->name = $this->name;
+        $simpan->email = $this->email;
+        if ($this->password) {
+            $simpan->password = bcrypt($this->password);
+        }
+        $simpan->peran = $this->peran;
+        $simpan->save();
+
+        $this->reset(['name', 'email', 'password', 'peran', 'penggunaTerpilih']);
+        $this->pilihanMenu = 'lihat';
     }
 }
